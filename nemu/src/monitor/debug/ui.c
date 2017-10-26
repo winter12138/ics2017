38b220f4b41282666e6c447c781990e8eb398f4d
tracer-ics2017 <tracer@njuics.org>
2017-10-26 17:15:04 +0800

    >  compile U201414924 winter Linux ubuntu 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:15:04 up 18:53,  2 users,  load average: 0.32, 0.16, 0.10 9224f7ba2890379651424e50c310d69dfc4886c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -103,9 +103,25 @@ static int cmd_x(char *args) {
 
 static int cmd_p(char *args) {
   bool success;
-  int a = expr(args, &success);
+  int a;
+  char *arg, *fmt="%d\n";
+
+  arg = strtok(NULL, " ");
+  if(NULL == arg) {
+    return 0;
+  }
+
+  if(0 == strcmp("x", arg)) {
+    arg = strtok(NULL, " ");
+    if(NULL == arg) {
+      return 0;
+    }
+    fmt = "%#x\n";
+  }
+
+  a = expr(arg, &success);
   if(true == success) {
-    printf("%d\n", a);
+    printf(fmt, a);
   } else {
     printf("Bad expression\n");
   }
