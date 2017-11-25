3c2875296a1f08f5ab67705ae1f0d597c3e278c4
tracer-ics2017 <tracer@njuics.org>
2017-11-25 10:50:10 +0800

    >  compile U201414924 winter Linux ubuntu 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  10:50:10 up 1 day,  9:27,  2 users,  load average: 0.88, 0.23, 0.20 f82591e8e0c6fb8958f0697038dcaa8cf212f6d

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -151,12 +151,23 @@ void difftest_step(uint32_t eip) {
   // Set `diff` as `true` if they are not the same.
   if(r.eax != cpu.eax || r.ecx != cpu.ecx || r.edx != cpu.edx || r.ebx != cpu.ebx
     || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi || r.edi != cpu.edi
-    || r.eip != cpu.eip )
+    || r.eip != cpu.eip || r.eflags != cpu.eflags)
   {
     diff = true;
-  }
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.eax", r.eax, "cpu.eax", cpu.eax);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.ecx", r.ecx, "cpu.ecx", cpu.ecx);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.edx", r.edx, "cpu.edx", cpu.edx);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.ebx", r.ebx, "cpu.ebx", cpu.ebx);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.esp", r.esp, "cpu.esp", cpu.esp);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.ebp", r.ebp, "cpu.ebp", cpu.ebp);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.esi", r.esi, "cpu.esi", cpu.esi);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.edi", r.edi, "cpu.edi", cpu.edi);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.eip", r.eip, "cpu.eip", cpu.eip);
+    printf("%-10s %-#10x %-10s %-#10x\n", "r.eflags", r.eflags, "cpu.eflags", cpu.eflags);
+ }
 
   if (diff) {
     nemu_state = NEMU_END;
+    printf("The states of nemu and qemu are inconsistent.\n");
   }
 }
