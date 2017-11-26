ccb88e6a01c5ad6ea51524927bb85c317d198cfc
tracer-ics2017 <tracer@njuics.org>
2017-11-26 18:45:48 +0800

    >  compile U201414924 winter Linux ubuntu 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:45:48 up 2 days,  5:03,  2 users,  load average: 0.15, 0.19, 0.30 1bef61e1afa73c4b3c36453f74e1e7c1273ca28f

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,13 +27,18 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(eip, 4);
+  int width;
+  width = decoding.is_operand_size_16 ? 2 : 4;
+  rtl_push(eip, width);
   decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(call_rm) {
+  int width;
+  width = decoding.is_operand_size_16 ? 2 : 4;
+  rtl_push(eip, width);
   decoding.jmp_eip = id_dest->val;
   if(decoding.is_operand_size_16)
     decoding.jmp_eip &= 0xffff;
@@ -44,7 +49,11 @@ make_EHelper(call_rm) {
 
 
 make_EHelper(ret) {
-  rtl_pop(&decoding.jmp_eip, 4);
+  int width;
+  width = decoding.is_operand_size_16 ? 2 : 4;
+  rtl_pop(&decoding.jmp_eip, width);
+  if(decoding.is_operand_size_16)
+    decoding.jmp_eip &= 0xffff;
   decoding.is_jmp = 1;
 
   print_asm("ret");
