bad37b14184a65e2ec6c9d44f4b7824399826849
tracer-ics2017 <tracer@njuics.org>
2017-11-26 17:13:32 +0800

    >  compile U201414924 winter Linux ubuntu 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:13:32 up 2 days,  3:31,  2 users,  load average: 0.75, 0.48, 0.39 cc08765f394c7bf4ca3f4ff2977fc2276e8050ef

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -151,16 +151,11 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  if(is_skip_eflags){
-    r.eflags &= 0x0;
-    cpu.eflags &= 0x0;
-    is_skip_eflags = false;
-  } else {
-    r.eflags &= 0xac3;
-  }
+  r.eflags &= 0xac3;
+
   if(r.eax != cpu.eax || r.ecx != cpu.ecx || r.edx != cpu.edx || r.ebx != cpu.ebx
     || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi || r.edi != cpu.edi
-    || r.eip != cpu.eip || r.eflags != cpu.eflags)
+    || r.eip != cpu.eip || ( !is_skip_eflags && r.eflags != cpu.eflags))
   {
     diff = true;
     printf("The states of nemu and qemu are inconsistent.\n");
@@ -174,7 +169,9 @@ void difftest_step(uint32_t eip) {
     printf("%-10s %-#10x %-10s %-#10x\n", "r.edi", r.edi, "cpu.edi", cpu.edi);
     printf("%-10s %-#10x %-10s %-#10x\n", "r.eip", r.eip, "cpu.eip", cpu.eip);
     printf("%-10s %-#10x %-10s %-#10x\n", "r.eflags", r.eflags, "cpu.eflags", cpu.eflags);
- }
+  }
+
+  is_skip_eflags = false;
 
   if (diff) {
     nemu_state = NEMU_END;
