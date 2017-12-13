e6191ae67d813393e3943c970529dacbb2f2307e
tracer-ics2017 <tracer@njuics.org>
2017-12-13 14:06:34 +0800

    >  compile U201414924 winter Linux ubuntu 4.4.0-103-generic #126-Ubuntu SMP Mon Dec 4 16:23:28 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:06:34 up  8:46,  1 user,  load average: 0.73, 0.57, 0.57 2bdbca77e16a4a7617987791582465931db00720

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,30 @@
 #include "common.h"
 #include "syscall.h"
 
+uintptr_t sys_none()
+{
+	return 1;
+}
+
+void sys_exit(int code)
+{
+	_halt(code);
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
+  	case SYS_none: {
+  		SYSCALL_ARG1(r) = sys_none();
+  		break;
+  	}
+  	case SYS_exit: {
+  		a[1] = SYSCALL_ARG2(r);
+  		sys_exit(a[1]);
+  		break;
+  	}
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
